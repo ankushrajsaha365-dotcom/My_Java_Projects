9cbcbc702b8f33d70bcd94632e46aecc23c63489
ankushrajsaha365-dotcom <ankushrajsaha365@gmail.com>
2026-04-03 23:43:25 +0530

    day1

---
+++ Day01/VariablesDemo.java
@@ -0,0 +1,12 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int age =20;
+    //    double CGPA = 9.59;
+    //    char grade  = 'A';
+    //   boolean isJavaFun = true;
+        String name = "Raj";
+
+        System.out.println("Hello there I am "+ name + ". " + age + " years old.");
+    }
+    
+}
