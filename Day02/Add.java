bf106c197bf8c99bcbf631d702b2b2143acec4ae
ankushrajsaha365-dotcom <ankushrajsaha365@gmail.com>
2026-05-06 23:30:03 +0530

    Java Prac

---
+++ Day02/Add.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Add {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number a: ");
+    int a = sc.nextInt();  
+    System.out.println("Enter a number b: ");
+    int b = sc.nextInt();
+    int c = a+b;
+    System.out.println("Addition = "+c);
+    sc.close();
+    }
+    
+
+}
+ 
+    
+
---
+++ Day02/Sum_of_num.java
@@ -0,0 +1,11 @@
+public class Sum_of_num {
+    public static void main(String[] args) {
+        int stop = 100 ;
+        int add =0;
+        for(int i = 0;i<=stop;i++){
+            add += i;
+        }
+        System.out.println("The sum of numbers upto range " +stop+ " is " + add);
+    }
+    
+}
